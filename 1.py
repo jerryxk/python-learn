ef4fb96ca6e781e80687ebaac32b93f288ba3ecf
jerryxk <xukai1468@126.com>
2017-06-01 23:38:12 +0800

    1111

--- 1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-a = 'py交易'
-print a
--- md5ts.py
+++
@@ -1,9 +0,0 @@
-from hashlib import md5
-
-s = '963ba5'
-# print s[:2]
-
-for i in range(9999999):
-	if(md5(str(i)).hexdigest()[:6] == s):
-		print i
-		break
